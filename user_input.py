ad061320830c478d8fc61f1e859c75be48834149
Janekabura21 <janenjoki2105@gmail.com>
2024-08-15 22:23:05 +0300

    Add initial Python files

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+# Ask the user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
